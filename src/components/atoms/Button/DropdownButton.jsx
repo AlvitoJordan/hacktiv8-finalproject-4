ca1dd3c5569cbbda41999cbbd6ce29738f151a55
Alvito Jordan <alvitojordan12@gmail.com>
2023-12-16 10:57:45 +0700

    Fix submit error & add search in detail page

--- src/components/atoms/Button/DropdownButton.jsx
+++ src/components/atoms/Button/DropdownButton.jsx
@@ -8,6 +8,7 @@ const DropdownButton = ({ options, onClick, select }) => {
   return (
     <div>
       <button
+        type="button"
         className="flex items-center justify-between border-r-2 p-2 w-28 gap-2 cursor-pointer border-gray-300 dark:border-gray-500 bg-transparent text-gray-400 focus:outline-none"
         onClick={dropdownToogle}
       >
@@ -20,6 +21,7 @@ const DropdownButton = ({ options, onClick, select }) => {
           <div>
             {options.map((option) => (
               <button
+                type="button"
                 key={option.id}
                 onClick={() => onClick(option)}
                 className="block w-28 py-2 text-gray-500 dark:text-gray-200 focus:outline-none hover:bg-gray-100 dark:hover:bg-gray-900 hover:text-gray-900"
--- src/components/templates/Layout.jsx
+++ src/components/templates/Layout.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import { NavBar } from "../organisms";
 import { Footer } from "../molecules";
 import Search from "../../requirement/Search";
+import { useNavigate } from "react-router-dom";
+import { DropdownHooks, SearchingHooks } from "../../hooks";
 
 const Layout = ({ children }) => {
-  const handleSearch = (searchValue) => {
-    console.log("Search Value:", searchValue);
+  const { setSearchValue } = SearchingHooks("");
+  const { setSelected } = DropdownHooks();
+  const navigate = useNavigate();
+  const handleSearch = (searchValue, selected) => {
+    setSearchValue(searchValue);
+    setSelected(selected);
+    navigate("/", { state: { searchValue, selected } });
   };
   return (
     <div className=" bg-white dark:bg-gradient-to-t dark:from-black dark:from-80%  dark:to-darkRed min-h-screen">
